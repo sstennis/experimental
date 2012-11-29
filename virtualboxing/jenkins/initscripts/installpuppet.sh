52bd10fb534129a804e02f2f9d7f8a8c8f7c4cba
Scot Stennis <scot.stennis@icix-usa.com>
2012-11-29 14:57:23 -0800

    US1451 : TA2193 Provision of VM for Jenkins (CI). Playing with ruby install to get past dependency error. Trial and error.

--- virtualboxing/jenkins/initscripts/installpuppet.sh
+++ virtualboxing/jenkins/initscripts/installpuppet.sh
@@ -42,7 +42,7 @@ gpgcheck=0
 EOF'
 
 sudo yum remove -y ruby ruby-irb ruby-rdoc
-#sudo yum install -y ruby ruby-irb ruby-rdoc
+sudo yum install -y ruby ruby-irb ruby-rdoc
 sudo yum --enablerepo="ruby" update -y ruby ruby-irb ruby-rdoc
 
 sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
