b7652a4e19f6230da25e047b1647c341f186435d
Scot Stennis <scot.stennis@icix-usa.com>
2012-11-30 15:49:30 -0800

    US1451 : TA2193 Provision of VM for Jenkins (CI). Changed copy call to prevent copy of modules directory and only copy the contents of the modules dir (creates links really).

--- virtualboxing/jenkins/initscripts/initprovision.sh
+++ virtualboxing/jenkins/initscripts/initprovision.sh
@@ -169,7 +169,7 @@ fi
 # This location is typically /etc/puppet/manifests and /etc/puppet/modules
 # Because of this, it is essential to use unique names for manifests in the root
 # manifest dir and modules in the modules dir.
-sudo cp -rl $REPOSITORY_DIRECTORY/virtualboxing/jenkins/puppet/modules /etc/puppet/modules
+sudo cp -rl $REPOSITORY_DIRECTORY/virtualboxing/jenkins/puppet/modules/* /etc/puppet/modules
 
 
 
