6319147f37e7ff8ff519579c974227f37b04406f
Scot Stennis <scot.stennis@icix-usa.com>
2012-11-30 14:06:00 -0800

    US1451 : TA2193 Provision of VM for Jenkins (CI). Added full path resolution.

--- virtualboxing/jenkins/initscripts/provisionjenkins.sh
+++ virtualboxing/jenkins/initscripts/provisionjenkins.sh
@@ -3,4 +3,6 @@
 #	Provision for Jenkins via puppet
 #
 
-sudo puppet apply ../puppet/manifests/baseCentOS56.pp
+THIS_DIRECTORY="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sudo puppet apply $THIS_DIRECTORY/../puppet/manifests/baseCentOS56.pp
